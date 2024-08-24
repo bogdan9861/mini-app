76a11361da1579782004aa9bfacd02a63c4fb8ca
bogdan9861 <besedin_b@bk.ru>
2024-08-24 22:09:55 +0300

    change secure field animation

--- src/components/Spoiler/Spoiler.jsx
+++ src/components/Spoiler/Spoiler.jsx
@@ -8,19 +8,50 @@ const Spoiler = ({ secureText, ...props }) => {
   const cvs = useRef();
 
   useEffect(() => {
+    let width = 100;
+    let height = 20;
     let ctx = cvs.current.getContext("2d");
-    
+    ctx.fillStyle = "#A5A5A5";
+
     let times = 0;
+
+    let positions = [];
+
+    for (let i = 0; i < 20; i++) {
+      let ranX = Math.floor(Math.random() * width);
+      let randY = Math.floor(Math.random() * height);
+
+      positions.push({ x: ranX, y: randY, index: i });
+      ctx.fillRect(positions[i].x, positions[i].y, 2, 2);
+    }
 
     requestAnimationFrame(function animate(time) {
       times++;
+
       for (let i = 0; i < 20; i++) {
-        ctx.fillStyle = "#A5A5A5";
+        if (i === positions[i].index) {
+          let nextStepX;
+          let nextStepY;
 
-        let rndX = Math.floor(Math.random() * 100);
-        let rndY = Math.floor(Math.random() * 20);
+          if (positions[i].x <= width && positions[i].x > 0) {
+            nextStepX = Math.floor(Math.random() * 2 - 1);
+          } else {
+            nextStepX = width - 2;
+          }
 
-        ctx.fillRect(rndX, rndY, 2, 2);
+          if (positions[i].y >= 0) {
+            nextStepY = Math.floor(Math.random() * 2 - 1);
+          } else {
+            nextStepY = height - 2;
+          }
+
+          positions[i] = {
+            x: positions[i].x + nextStepX,
+            y: positions[i].y + nextStepY,
+            index: i,
+          };
+          ctx.fillRect(positions[i].x + 1, positions[i].y + 1, 2, 2);
+        }
 
         if (times > 1) {
           times = 0;
